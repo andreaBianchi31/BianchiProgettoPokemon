2834159a0d91081d39493841f4141a9854a753a8
Andrea Bianchi <andrea.bianchi@marconirovereto.it>
2023-04-06 11:13:13 +0200

    Modifiche a Pokemon e Pokedex

--- BianchiProgettoPokemon/src/app/common/pokedex.service.ts
+++ BianchiProgettoPokemon/src/app/common/pokedex.service.ts
@@ -1,9 +1,79 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Pokemon } from './pokemon';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PokedexService {
+export class PokedexService
+{
+  baseURL: string = 'https://pokeapi.co/api/v2/';
+  searchPokemon: string = this.baseURL + 'pokemon/';
+  searchPokemonSpecies: string = this.baseURL + 'pokemon-species/';
+  searchType: string = this.baseURL + 'type/';
+  searchEvolutionChain: string = this.baseURL + 'evolution-chain/';
+  searchForms: string = this.baseURL + 'pokemon-form/';
 
-  constructor() { }
+  listaPokemon: Pokemon[] = [];
+  //currentPokemon: Pokemon = this.getPokemonByID('pikachu');
+
+
+  constructor(private httpAssistant: HttpClient)
+  {
+  }
+
+
+  getPokemonList(): Pokemon[]
+  {
+    return this.listaPokemon;
+  }
+
+
+  /*getCurrentPokemon(): Pokemon
+  {
+    //return this.currentPokemon;
+  }*/
+
+
+  getPokemonByID(id: string): Observable<any>
+  {
+    return this.httpAssistant.get(this.searchPokemon + id);
+  }
+
+
+  getPokemonByURL(url: string): Observable<any>
+  {
+    return this.httpAssistant.get(url);
+  }
+
+
+  getPokemonSpeciesByID(id: string): Observable<any>
+  {
+    return this.httpAssistant.get(this.searchPokemonSpecies + id);
+  }
+
+
+  getPokemonSpeciesByURL(url: string): Observable<any>
+  {
+    return this.httpAssistant.get(url);
+  }
+
+
+  getPokemonByType(type: string): Observable<any>
+  {
+    return this.httpAssistant.get(this.searchType + type);
+  }
+
+
+  getPokemonByEvolutionChian(evolutionChain: string): Observable<any>
+  {
+    return this.httpAssistant.get(this.searchEvolutionChain + evolutionChain);
+  }
+
+
+  getPokemonForms(name: string)
+  {
+
+  }
 }
--- BianchiProgettoPokemon/src/app/common/pokemon.ts
+++ BianchiProgettoPokemon/src/app/common/pokemon.ts
@@ -1,2 +1,68 @@
-export class Pokemon {
+export class Pokemon
+{
+    name: string;
+    description: string;
+    id: number;
+    pokedexNumber: number;
+    forms: Pokemon[];
+    category: string; //genera
+    generation: string;
+
+    healthPoints: number;
+    attack: number;
+    defence: number;
+    speed: number;
+    specialAttack: number;
+    specialDefence: number;
+
+    type1: string;
+    type2: string;
+
+    isBaby: boolean;
+    isLegendary: boolean;
+    isMythical: boolean;
+
+    evolutionChain: string;
+    flavorText: string;
+    description: string;
+
+
+    constructor(name: string, description: string,
+        id: number,
+        pokedexNumber: number,
+        forms: Pokemon[],
+        category: string,
+        generation: string,
+        healthPoints: number,
+        attack: number,
+        defence: number,
+        speed: number,
+        specialAttack: number,
+        specialDefence: number,
+        type1: string,
+        type2: string,
+        isLegendary: boolean,
+        isMythical: boolean)
+        {
+            this.name = name;
+            this.description = description;
+            this.id = id;
+            this.pokedexNumber = pokedexNumber;
+            this.forms = forms;
+            this.category = category;
+            this.generation = generation;
+
+            this.healthPoints = healthPoints;
+            this.attack = attack;
+            this.defence = defence;
+            this.speed = speed;
+            this.specialAttack = specialAttack;
+            this.specialDefence = defence;
+
+            this.type1 = type1;
+            this.type2 = type2;
+
+            this.isLegendary = isLegendary;
+            this.isMythical = isMythical;
+        }
 }
