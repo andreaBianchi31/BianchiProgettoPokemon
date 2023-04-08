fa3ec1f16221af73fe068a9fd097952b06041ba0
Andrea Bianchi <andrea.bianchi@marconirovereto.it>
2023-04-08 15:00:01 +0200

    Create cartelle per le immagini

--- BianchiProgettoPokemon/src/app/cool-card/cool-card.component.ts
+++ BianchiProgettoPokemon/src/app/cool-card/cool-card.component.ts
@@ -15,9 +15,9 @@ export class CoolCardComponent
 
   constructor()
   {
-    this.title = 'Titolo';
+    this.title = 'Pokémon Blue Version';
     this.text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam a velit vitae nunc gravida vestibulum eu at mi. Aenean nec dui vitae lectus gravida volutpat a at diam. Suspendisse nisl dolor, ornare vitae erat non, hendrerit ultricies neque. Quisque iaculis tempor mauris sit amet dignissim. Aenean varius sagittis pretium. Cras aliquam est porttitor tempus interdum. Fusce congue egestas bibendum. Quisque ac leo sit amet turpis euismod vestibulum.';
-    this.imagePath = 'https://c4.wallpaperflare.com/wallpaper/448/174/357/neon-4k-hd-best-for-desktop-wallpaper-preview.jpg';
+    this.imagePath = '../assets/images/types/pokemon_blue_version__game_boy__hq_box_art_by_jadelune_d9tkraf-fullview.jpg';
     this.buttonText = 'Bottone';
     this.buttonLink = 'https://www.marconirovereto.it/';
   }
