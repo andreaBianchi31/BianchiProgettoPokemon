81c710e1bc7f479318a04749957b2df756f99706
Andrea Bianchi <andrea.bianchi@marconirovereto.it>
2023-05-17 15:04:23 +0200

    Commit Prova (CONTROLLARE ERRORE GET)

--- BianchiProgettoPokemon/src/app/maxi-container/maxi-container.component.ts
+++ BianchiProgettoPokemon/src/app/maxi-container/maxi-container.component.ts
@@ -17,19 +17,21 @@ export class MaxiContainerComponent
   generation: number = 1;
   generationList: number[] = [1, 2, 3, 4, 5, 6, 7, 8];
 
-  game: number = 2;
+  game: string = '2';
 
   parameter: string = 'generation';
 
   datiDisponibili: boolean;
   cambioForma: boolean;
 
+  tmp: number = 0;
+
 
   constructor(private pokedex: PokedexService, private title: Title)
   {
     this.title.setTitle('Pokedex - Home');
     this.generation = 1;
-    this.game = 2;
+    this.game = '2';
     this.getPokemonByGeneration();
     this.datiDisponibili = false;
     this.cambioForma = false;
@@ -38,6 +40,8 @@ export class MaxiContainerComponent
 
   getPokemonByGeneration()
   {
+    
+    
     if (this.generation == 0)
     {
       console.log('Searching pokemon of All Generations...');
@@ -51,13 +55,17 @@ export class MaxiContainerComponent
             this.pokemonList = [];
             let pokemonSpeciesList = data.results;
 
+            this.tmp = pokemonSpeciesList.length;
+
             pokemonSpeciesList.forEach((pokemon: any, index: number) => {
                 this.pokedex.getPokemonSpeciesByUrl(pokemon.url).subscribe(
                   (data: any) => {
                     let pokemon = new PokemonSpecies(data.id, data.names, data.pokedex_numbers, data.is_baby, data.is_legendary, data.is_mythical, data.flavor_text_entries, data.form_descriptions, data.genera, data.generation.name, data.varieties, this.pokedex);
                     this.pokemonList.push(pokemon);
 
-                    if (pokemonSpeciesList.length == this.pokemonList.length)
+                    console.log('ses');
+
+                    if (this.tmp == this.pokemonList.length)
                     {
                       this.datiDisponibili = true;
                       this.pokemonList = this.pokedex.sortPokemonSpeciesList(this.pokemonList);
@@ -65,6 +73,10 @@ export class MaxiContainerComponent
                       console.log(this.pokemonList);
                       this.getAllVarieties();
                     }
+                  },
+                  (error: any) => {
+                    this.tmp -= 1;
+                    console.log(this.tmp);
                   }
                 )
             });
@@ -87,13 +99,17 @@ export class MaxiContainerComponent
             this.pokemonList = [];
             let pokemonSpeciesList = data.pokemon_species;
 
+            this.tmp = pokemonSpeciesList.length;
+
+
+            // =======> controllo
             pokemonSpeciesList.forEach((pokemon: any) => {
               this.pokedex.getPokemonSpeciesByUrl(pokemon.url).subscribe(
                 (data: any) => {
                   let pokemon = new PokemonSpecies(data.id, data.names, data.pokedex_numbers, data.is_baby, data.is_legendary, data.is_mythical, data.flavor_text_entries, data.form_descriptions, data.genera, data.generation.name, data.varieties, this.pokedex);
                   this.pokemonList.push(pokemon);
 
-                  if (pokemonSpeciesList.length == this.pokemonList.length)
+                  if (this.tmp == this.pokemonList.length)
                   {
                     this.datiDisponibili = true;
                     this.pokemonList = this.pokedex.sortPokemonSpeciesList(this.pokemonList);
@@ -101,6 +117,10 @@ export class MaxiContainerComponent
                     console.log(this.pokemonList);
                     this.getAllVarieties();
                   }
+                },
+                (error: any) => {
+                  this.tmp -= 1;
+                  console.log(this.tmp);
                 }
               )
             });
@@ -109,6 +129,15 @@ export class MaxiContainerComponent
           console.log('Failed search!');
         }
       );
+
+      if (this.tmp == this.pokemonList.length)
+      {
+        this.datiDisponibili = true;
+        this.pokemonList = this.pokedex.sortPokemonSpeciesList(this.pokemonList);
+        console.log(this.pokemonList.length + ' pokemon species found!');
+        console.log(this.pokemonList);
+        this.getAllVarieties();
+      }
     }
   }
 
@@ -125,34 +154,53 @@ export class MaxiContainerComponent
   getPokemonByGameVersion()
   {
     let tmp = 'Pokédex - ';
-    if (this.game == 1)
-    tmp += 'National';
+
     console.log(this.game);
-    /*
+    
     switch(this.game) {
-      case 1: tmp += 'National'; break;
-      case 2: tmp += 'Red | Blue | Yellow'; console.log(tmp); break;
-      case 3: tmp += 'Gold | Silver | Crystal'; break;
-      case 4: tmp += 'Ruby | Sapphire | Emerald'; break;
-      case 5: tmp += 'Diamond | Pearl'; break;
-      case 6: tmp += 'Platinum'; break;
-      case 7: tmp += 'HeartGold | SoulSilver'; break;
-      case 8: tmp += 'Black | White'; break;
-      case 9: tmp += 'Black 2 | White 2'; break;
-      case 11: tmp += 'Pokemon Conquest'; break;
-      case 12: tmp += 'X | Y (plain)'; break;
-      case 13: tmp += 'X | Y (coast)'; break;
-      case 14: tmp += 'National'; break;
-      case 15: tmp += 'National'; break;
-      case 16: tmp += 'Sun | Moon'; break;
-      case 21: tmp += 'UltraSun | UltraMoon'; break;
-      case 26: tmp += 'Let\'s Go Pikachu | Eevee'; break;
-      case 27: tmp += 'Sword | Shield'; break;
-      case 28: tmp += 'Isle of Armoral'; break;
-      case 29: tmp += 'Chrown Tundra'; break;
-      case 30: tmp += 'Legends: Arceus'; break;
-      default: tmp += this.game; break;
-    }*/
+      case '1':
+        tmp += 'National'; break;
+      case '2':
+        tmp += 'Red | Blue | Yellow'; break;
+      case '3':
+        tmp += 'Gold | Silver | Crystal'; break;
+      case '4':
+        tmp += 'Ruby | Sapphire | Emerald'; break;
+      case '5':
+        tmp += 'Diamond | Pearl'; break;
+      case '6':
+        tmp += 'Platinum'; break;
+      case '7':
+        tmp += 'HeartGold | SoulSilver'; break;
+      case '8':
+        tmp += 'Black | White'; break;
+      case '9':
+        tmp += 'Black 2 | White 2'; break;
+      case '11':
+        tmp += 'Pokemon Conquest'; break;
+      case '12':
+        tmp += 'X | Y (plain)'; break;
+      case '13':
+        tmp += 'X | Y (coast)'; break;
+      case '14':
+        tmp += 'National'; break;
+      case '15':
+        tmp += 'National'; break;
+      case '16':
+        tmp += 'Sun | Moon'; break;
+      case '21':
+        tmp += 'UltraSun | UltraMoon'; break;
+      case '26':
+        tmp += 'Let\'s Go Pikachu | Eevee'; break;
+      case '27':
+        tmp += 'Sword | Shield'; break;
+      case '28':
+        tmp += 'Isle of Armoral'; break;
+      case '29':
+        tmp += 'Chrown Tundra'; break;
+      case '30':
+        tmp += 'Legends: Arceus'; break;
+    }
     this.title.setTitle(tmp);
 
     this.pokemonList = [];
@@ -174,7 +222,7 @@ export class MaxiContainerComponent
                     this.datiDisponibili = true;
                     this.pokemonList = this.pokedex.sortPokemonSpeciesList(this.pokemonList);
 
-                    if (this.game == 1) {
+                    if (this.game == '1') {
                       this.pokemonList = this.pokemonList.splice(0, this.pokedex.getLastPokemon());
                     }
 
@@ -253,12 +301,15 @@ export class MaxiContainerComponent
     {
       case 'generation':
         this.getPokemonByGeneration();
+        this.tmp = 0;
         break;
       case 'game-versions':
         this.getPokemonByGameVersion();
+        this.tmp = 0;
         break;
       case 'favourites':
         this.pokedex.reloadFavouriteList();
+        this.tmp = 0;
         this.getPokemonByFavourites();
         break;
     }
@@ -272,6 +323,16 @@ export class MaxiContainerComponent
     this.pokedex.clearFavouriteList();
     this.getPokemonByFavourites();
     this.datiDisponibili = true;
+  }
+
+
+  prova()
+  {
+    this.pokedex.getPokemonSpeciesByUrl('https://pokeapi.co/api/v2/pokemon-species/125/').subscribe(
+      (data) => {
+        console.log(data);
+      }
+    );
   }
 
 }
